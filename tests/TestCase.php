d3ac9ca24cda8bc4d6c069dd31e61f85d0fc3abf
Niek Poorthuis <niek@appcenternederland.nl>
2025-09-16 15:31:10 +0200

    - changes to TestCase.php

--- tests/TestCase.php
+++ tests/TestCase.php
@@ -77,4 +77,12 @@ class TestCase extends Orchestra
             ->middleware([AddQueuedCookiesToResponse::class, TrackPageView::class])
             ->name('test.post');
     }
+
+    protected function tearDown(): void
+    {
+        parent::tearDown();
+
+        restore_error_handler();
+        restore_exception_handler();
+    }
 }
