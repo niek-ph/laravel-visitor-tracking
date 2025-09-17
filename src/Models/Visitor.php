67c4529e7f460ecb9be10e82b8ed58210c441f06
Niek Poorthuis <niek@appcenternederland.nl>
2025-09-17 12:03:14 +0200

    - added fromRequest to visitor model

--- src/Models/Visitor.php
+++ src/Models/Visitor.php
@@ -2,12 +2,16 @@
 
 namespace NiekPH\LaravelVisitorTracking\Models;
 
+use DeviceDetector\ClientHints;
+use DeviceDetector\DeviceDetector;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\BelongsTo;
 use Illuminate\Database\Eloquent\Relations\HasMany;
 use Illuminate\Foundation\Auth\User;
+use Illuminate\Http\Request;
 use NiekPH\LaravelVisitorTracking\Database\Factories\VisitorFactory;
+use NiekPH\LaravelVisitorTracking\VisitorTag;
 use NiekPH\LaravelVisitorTracking\VisitorTracking;
 
 class Visitor extends Model
@@ -59,5 +63,37 @@ class Visitor extends Model
     public function events(): HasMany
     {
         return $this->hasMany(VisitorTracking::$eventModel);
+    }
+
+    /**
+     * Creates or retrieves an instance based on the provided request.
+     *
+     * @param  Request  $request  The HTTP request instance containing the incoming client data.
+     * @return self The created or retrieved instance populated with data extracted from the request.
+     */
+    public function fromRequest(Request $request): self
+    {
+        $tag = new VisitorTag()->retrieve($request);
+        $clientHints = config('visitor-tracking.enable_client_hints') ? ClientHints::factory($_SERVER) : null;
+
+        $user = $request->user();
+        $ipAddress = $request->ip();
+        $userAgent = $request->userAgent();
+
+        $deviceDetector = new DeviceDetector($userAgent, $clientHints);
+        $deviceDetector->parse();
+
+        return static::firstOrCreate([
+            'tag' => $tag,
+            'user_id' => $user?->id,
+            'user_agent' => $userAgent,
+            'ip_address' => $ipAddress,
+            'is_bot' => $deviceDetector->isBot(),
+            'device' => $deviceDetector->getDeviceName(),
+            'browser' => $deviceDetector->getClient('name'),
+            'platform' => $deviceDetector->getOs('name'),
+            'platform_version' => $deviceDetector->getOs('version'),
+        ]
+        );
     }
 }
